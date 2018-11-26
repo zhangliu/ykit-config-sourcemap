21920f9fb2b9a469d592710880d7362e7c7ce61f
zhangliu <zhangliuge@yeah.net>
2018-11-26 13:27:24 +0800

    modify async to co

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const SentryHelper = require('./sentryHelper')
 const fs = require('fs')
+const co = require('co')
 
 let options
 
 module.exports = {
     config: (opts, cwd) => options = opts,
     hooks: {
-        afterPack: async (cliParams, stats) => {
+        afterPack: co.wrap(function *(cliParams, stats) {
             const {hooks} = options || {}
             console.log(`获取到 sroucemap 配置：`, hooks)
             if (!hooks) return
@@ -20,22 +21,22 @@ module.exports = {
                 // })
     
                 console.log('开始生成 sentry 版本号...')
-                version = await sentry.createVersion(hooks.version)
+                version = yield sentry.createVersion(hooks.version)
                 console.log(`使用 sentry 版本号：${version}`)
 
                 console.log('清空该版本下的所有旧文件...')
-                await sentry.deleteFiles(version)
+                yield sentry.deleteFiles(version)
     
                 console.log('开始上传压缩文件到 sentry 系统...')
-                await sentry.uploadFiles(version, `${process.cwd()}/prd`)
+                yield sentry.uploadFiles(version, `${process.cwd()}/prd`)
                 console.log('开始上传 sourcemap 文件到 sentry 系统...')
-                await sentry.uploadFiles(version, `${process.cwd()}/prd_sourcemap`)
+                yield sentry.uploadFiles(version, `${process.cwd()}/prd_sourcemap`)
                 console.log(`上传至版本 ${version} 成功！`)
             } catch (e) {
                 console.error('上传文件至 sentry 失败！', e)
-                await sentry.deleteVersion(version)
+                yield sentry.deleteVersion(version)
                 throw e
             }
-        }
+        })
     }
 }
--- sentryHelper.js
+++ sentryHelper.js
@@ -1,5 +1,6 @@
 const exec = require('child_process').exec
 const path = require('path')
+const co = require('co')
 
 const EIGHT_HOURS = 8 * 3600 * 1000
 const versionGen = () => new Date(Date.now() + EIGHT_HOURS).toISOString().replace(/[^\d]/g, '')
@@ -13,7 +14,7 @@ class SentryHelper{
         this.cmdPre = `${cli} --url ${sentryService} --auth-token ${token} releases -o ${org} -p ${project}`
     }
 
-    async createVersion(version) {
+    createVersion(version) {
         return new Promise((resolve, reject) => {
             const cmd = `${this.cmdPre} new ${version}`
             exec(cmd, (error, stdout) => {
@@ -23,7 +24,7 @@ class SentryHelper{
         })
     }
 
-    async deleteFiles(version) {
+    deleteFiles(version) {
         return new Promise((resolve, reject) => {
             const cmd = `${this.cmdPre} files ${version} delete --all`
             exec(cmd, (error, stdout) => {
@@ -33,8 +34,8 @@ class SentryHelper{
         })
     }
 
-    async deleteVersion(version) {
-        if (!version) return null
+    deleteVersion(version) {
+        if (!version) return Promise.resolve(null)
         return new Promise((resolve, reject) => {
             const cmd = `${this.cmdPre} delete ${version}`
             exec(cmd, (error, stdout) => {
@@ -44,7 +45,7 @@ class SentryHelper{
         })
     }
 
-    async uploadFiles(version, filePath) {
+    uploadFiles(version, filePath) {
         return new Promise((resolve, reject) => {
             console.log(`开始上传 ${filePath} 下的文件...`)
             const cmd = `${this.cmdPre} files ${version} upload-sourcemaps --validate --url-prefix '${this.urlPrefix}' ${filePath}`
